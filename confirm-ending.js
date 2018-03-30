5bb597e8d7c5cd09f4312ca6945604f24cb27f1e
Fabian Lee <chleefabian@gmail.com>
2018-03-30 16:24:00 +0800

    Update confirm-ending.js

--- confirm-ending.js
+++ confirm-ending.js
@@ -1,5 +1,9 @@
-// This function checks if a string ends with a targeted string
-
+/**
+ * Checks if the string ends with the target string.
+ * @param {string} str
+ * @param {string} target
+ * @return {boolean} The index of the target element within the array.
+ */
 function confirmEnding(str, target) {
   str = str.replace(/[^A-Za-z0-9]/g, '');
   var endChar = str.substring(str.length - target.length);
