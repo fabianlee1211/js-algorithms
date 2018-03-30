d7ad84aeb48d2e0303abe6dbac82383ce47efd63
Fabian Lee <chleefabian@gmail.com>
2018-03-30 20:23:42 +0800

    Update longest-word.js

--- longest-word.js
+++ longest-word.js
@@ -1,11 +1,15 @@
-// This function finds the longest word in a sentence.
-
+/**
+ * Searches for the longest word in a string
+ * @param {string} str
+ * @return {string} max The longest word in the string.
+ */
 function findLongestWord(str) {
   var splitArray = str.split(' ');
   var max = 0;
   
   for(var i=0; i<splitArray.length; i++){
-    if(max < splitArray[i].length) max = splitArray[i].length;
+    if(max < splitArray[i].length) max = splitArray[i];
   }
+  max = max.replace('/[^a-zA-Z0-9]/g', '')
   return max;
 }
