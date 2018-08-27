871999cd67e5682a98b91bf53dd99ba51866ab81
Fabian Lee <chleefabian@gmail.com>
2018-08-27 09:27:56 +0800

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,16 +1,17 @@
-let arr = [15,2,7,4,1,10,9,8];
-
+/**
+ * Bubble sort algorithm
+ * @param {array} numbers
+ * @return {array} The sorted array
+ */
 function bubbleSort(numbers) {
   for(let i = 0; i < numbers.length; i++) {
-    // console.log('Outer count: ' + i );
     for(let j = 0; j < numbers.length - i - 1; j++) {
-      // console.log('Inner count: ' + j );
       let temp = numbers[j];
       if(numbers[j] > numbers[j+1]) {
         numbers[j] = numbers[j+1];
         numbers[j+1] = temp;
-        // console.log(numbers);
       } else continue;
     }
   }
+  return numbers;
 }
