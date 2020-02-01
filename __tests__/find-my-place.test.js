6504b50a5fde8fadb3488a9ccb5abfa28cd291f3
fabianlee1211 <chleefabian@gmail.com>
2020-02-01 21:57:34 +0800

    fix: find my place

--- __tests__/find-my-place.test.js
+++ __tests__/find-my-place.test.js
@@ -1,6 +1,11 @@
 const findMyPlace = require('../src/find-my-place');
 
 describe('Find My Place in an Array', () => {
+  it('should return index 0 with empty array', () => {
+    const result = findMyPlace([], 1);
+    expect(result).toEqual(0);
+  });
+
   it('should return the correct index in between values', () => {
     const result = findMyPlace([1, 2, 3, 4], 2);
     expect(result).toEqual(1);
@@ -9,11 +14,6 @@ describe('Find My Place in an Array', () => {
   it('should return the correct index with unordered values', () => {
     const result = findMyPlace([-4, -22, 10.2, -999], -3);
     expect(result).toEqual(3);
-  });
-
-  it('should return the first index', () => {
-    const result = findMyPlace([1, 2, 3, 4], -999);
-    expect(result).toEqual(0);
   });
 
   it('should return the last index', () => {
