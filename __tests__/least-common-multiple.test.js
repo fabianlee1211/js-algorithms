3d4edc3809c86d2806fa3b36b54804a874c2ad2c
Fabian Lee <chleefabian@gmail.com>
2020-02-22 20:55:38 +0800

    feat: sum of primes (#10)
    * feat: sum of primes
    
    * fix: remove log

--- __tests__/least-common-multiple.test.js
+++ __tests__/least-common-multiple.test.js
@@ -1,4 +1,7 @@
-const leastCommonMultiple = require('../src/least-common-multiple');
+const {
+  leastCommonMultiple,
+  greatestCommonFactor,
+} = require('../src/least-common-multiple');
 
 describe('Least Common Multiple', () => {
   it('should return undefined with zero', () => {
@@ -14,5 +17,17 @@ describe('Least Common Multiple', () => {
   it('should return LCM correctly', () => {
     const result = leastCommonMultiple([1, 5]);
     expect(result).toEqual(60);
+  });
+});
+
+describe('Greatest Common Factor', () => {
+  it('should return GCF correctly', () => {
+    const result = greatestCommonFactor([50, 125]);
+    expect(result).toEqual(25);
+  });
+
+  it('should return GCF of two equal numbers correctly', () => {
+    const result = greatestCommonFactor([5, 5]);
+    expect(result).toEqual(5);
   });
 });
