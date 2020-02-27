013f756b57cfac3a907c2a9901e62975d91ee067
Fabian Lee <chleefabian@gmail.com>
2020-02-28 01:28:52 +0800

    refactor: comment and variable (#13)

--- src/roman-converter.js
+++ src/roman-converter.js
@@ -1,20 +1,10 @@
+/**
+ * Convert Arabic to Roman numbers
+ * @param {number} number
+ * @return {string} The roman number
+ */
 module.exports = function romanConverter(number) {
-  if (number <= 0) return undefined;
-  const romanNumerals = {
-    1000: 'M',
-    900: 'CM',
-    500: 'D',
-    400: 'CD',
-    100: 'C',
-    90: 'XC',
-    50: 'L',
-    40: 'XL',
-    10: 'X',
-    9: 'IX',
-    5: 'V',
-    4: 'IV',
-    1: 'I',
-  };
+  if (number < 1) return undefined;
   const convertedDigits = [];
   // Object entries sorts itself =_=
   const romanNumeralKeyValues = Object.entries(romanNumerals).reverse();
@@ -26,4 +16,20 @@ module.exports = function romanConverter(number) {
   }
   const convertedString = convertedDigits.join('');
   return convertedString;
+};
+
+const romanNumerals = {
+  1000: 'M',
+  900: 'CM',
+  500: 'D',
+  400: 'CD',
+  100: 'C',
+  90: 'XC',
+  50: 'L',
+  40: 'XL',
+  10: 'X',
+  9: 'IX',
+  5: 'V',
+  4: 'IV',
+  1: 'I',
 };
