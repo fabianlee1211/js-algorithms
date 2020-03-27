23d1ee0df0d4e95a4f72e9d0109cc72e20ec33d3
fabianlee1211 <chleefabian@gmail.com>
2020-03-28 00:24:39 +0800

    fix: add comment

--- src/add-together.js
+++ src/add-together.js
@@ -1,3 +1,8 @@
+/**
+ * Find my place in this sorted array.
+ * @param {any} thisArgs - Any kind of arguments
+ * @return {number} The sum of the two values
+ */
 module.exports = function addTogether(...thisArgs) {
   const thisSum = thisArgs.reduce((prev, curr) => {
     if (typeof curr !== 'number') return undefined;
